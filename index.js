20b8b43b78daead78267d3e76244c7f18c39a5c8
Kevin Koech <kevinkoech007@gmail.com>
2025-06-18 23:34:54 +0300

    complete test

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+
+const employee = {
+  name: 'Sam',
+  streetAddress: '11 Broadway'
+};
+
+//updateEmployeeWithKeyAndValue()
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value
+
+  };
+}
+
+//destructivelyUpdateEmployeeWithKeyAndValue()
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+};
+
+// deleteFromEmployeeByKey()
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = {...employee};
+  delete newEmployee[key];
+  return newEmployee;
+};
+
+// destructivelyDeleteFromEmployeeByKey()
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+};
